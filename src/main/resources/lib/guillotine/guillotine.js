47695320ab283e804a984c8b08947f42a609a83f
Glenn Ricaud <gri@enonic.com>
2018-01-15 12:25:51 +0100

    Guillotine lib - Library Skeleton & Create Content API (#3)

--- src/main/resources/lib/guillotine/guillotine.js
+++ src/main/resources/lib/guillotine/guillotine.js
@@ -1,3 +1,32 @@
 var contentApiLib = require('./content-api');
 
-exports.createContentApi = contentApiLib.createContentApiType;
+exports.createContentApi = function (context) {
+    return contentApiLib.createContentApiType(context || createContext());
+};
+
+exports.createContext = createContext;
+
+function createContext() {
+    return {
+        types: {},
+        dictionary: [],
+        nameSet: {},
+        contentTypeMap: {},
+        addObjectType: function (objectType) {
+            this.dictionary.push(objectType);
+        },
+        putContentType: function (name, objectType) {
+            this.contentTypeMap[name] = objectType;
+        },
+        uniqueName: function (name) {
+            var uniqueName = name;
+            if (this.nameSet[name]) {
+                this.nameSet[uniqueName]++;
+                uniqueName = name + '_' + this.nameSet[uniqueName];
+            } else {
+                this.nameSet[uniqueName] = 1;
+            }
+            return uniqueName;
+        }
+    };
+}
