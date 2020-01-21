026bdca58da35469de009000a860097366ea6521
GlennRicaud <gri@enonic.com>
2020-01-21 16:20:18 +0100

    Guillotine Lib - Subscription - New event subscription #36

--- src/main/resources/lib/guillotine/root-subscription.js
+++ src/main/resources/lib/guillotine/root-subscription.js
@@ -8,7 +8,22 @@ exports.createRootSubscriptionType = function (context) {
         fields: {
             newEvent: {
                 type: eventLib.createEventObjectType(context),
-                resolve: () => newEventPublisher
+                args: {
+                    type: graphQlLib.GraphQLString,
+                    localOnly: graphQlLib.GraphQLBoolean
+                },
+                resolve: (env) => {
+                    if (env.args.type || env.args.localOnly) {
+                        let regexp = null;
+                        if (env.args.type) {
+                            const pattern = env.args.type.replace(/\./g, '\\.').replace(/\*/g, '.*');
+                            regexp = new RegExp('^' + pattern + '$');
+                        }
+                        return newEventPublisher.filter(
+                            (event) => (!env.args.localOnly || event.localOrigin) && (!regexp || regexp.test(event.type)))
+                    }
+                    return newEventPublisher;
+                }
             }
         }
     });
