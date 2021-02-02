fbcd9eb946e0e334ec9e214afb90bdb840caebd8
Anatol Sialitski <asi@enonic.com>
2021-02-02 10:48:07 +0300

    Filter Subscriptions #97

--- src/main/resources/lib/guillotine/index.js
+++ src/main/resources/lib/guillotine/index.js
@@ -4,6 +4,8 @@ const dynamicLib = require('/lib/guillotine/dynamic/index');
 const graphQlLib = require('/lib/guillotine/graphql');
 const rootQueryLib = require('/lib/guillotine/query/root-query');
 const rootSubscriptionLib = require('/lib/guillotine/subscription/root-subscription');
+const graphQlRxLib = require('/lib/graphql-rx');
+const webSocketLib = require('/lib/xp/websocket');
 
 function createSchema(options) {
     const context = createContext(options);
@@ -65,10 +67,93 @@ function createContext(options) {
     }
     context.options.applications = context.options.applications || [app.name];
     context.options.allowPaths = context.options.allowPaths || [];
+    context.options.subscriptionEventTypes = context.options.subscriptionEventTypes || ['node.*'];
 
     return context;
+}
+
+//──────────────────────────────────────────────────────────────────────────────
+// GraphQL WS Protocol
+//──────────────────────────────────────────────────────────────────────────────
+const graphQlSubscribers = {};
+
+function cancelSubscription(sessionId) {
+    Java.synchronized(() => {
+        const subscriber = graphQlSubscribers[sessionId];
+        if (subscriber) {
+            delete graphQlSubscribers[sessionId];
+            subscriber.cancelSubscription();
+        }
+    }, graphQlSubscribers)();
+}
+
+function handleStartMessage(schema, sessionId, message) {
+    const graphQLOperationId = message.id;
+    const payload = message.payload;
+
+    try {
+        const result = graphQlLib.execute(schema, payload.query, payload.variables);
+
+        if (result.data instanceof com.enonic.lib.graphql.rx.Publisher) {
+            const subscriber = graphQlRxLib.createSubscriber({
+                onNext: (result) => {
+                    webSocketLib.send(sessionId, JSON.stringify({
+                        type: 'data',
+                        id: graphQLOperationId,
+                        payload: result
+                    }));
+                }
+            });
+            Java.synchronized(() => graphQlSubscribers[sessionId] = subscriber, graphQlSubscribers)();
+            result.data.subscribe(subscriber);
+        }
+    } catch (e) {
+        log.error('Error while handling Start GraphQL-WS message', e);
+        throw e;
+    }
+}
+
+function initWebSockets(schema) {
+    return function (event) {
+        switch (event.type) {
+        case 'close':
+            cancelSubscription(event.session.id);
+            break;
+        case 'message':
+            processEventMessage(schema, event);
+            break;
+        case 'error':
+            log.warning('Session [' + event.session.id + '] error: ' + event.error);
+            break;
+        default:
+            log.debug(`Unknown event type ${event.type}`);
+            break;
+        }
+    }
+}
+
+function processEventMessage(schema, event) {
+    var message = JSON.parse(event.message);
+    switch (message.type) {
+    case 'connection_init':
+        webSocketLib.send(event.session.id, JSON.stringify({
+            type: 'connection_ack'
+        }));
+        break;
+    case 'start':
+        handleStartMessage(schema, event.session.id, message);
+        break;
+    case 'stop':
+        cancelSubscription(event.session.id);
+        break;
+    default:
+        log.debug(`Unknown message type ${message.type}`);
+        break;
+    }
 }
 
 exports.createSchema = createSchema;
 exports.createHeadlessCmsType = createContentApi;
 exports.createContext = createContext;
+exports.initWebSockets = initWebSockets;
+
--- src/main/resources/lib/guillotine/subscription/event.js
+++ src/main/resources/lib/guillotine/subscription/event.js
@@ -1,6 +1,7 @@
 const graphQlLib = require('/lib/graphql');
 const graphQlRxLib = require('/lib/graphql-rx');
 const eventLib = require('/lib/xp/event');
+const ctxLib = require('/lib/xp/context');
 
 function createEventObjectType(context) {
     return graphQlLib.createObjectType({
@@ -27,16 +28,51 @@ function createEventObjectType(context) {
 }
 
 function createNewEventPublisher(context) {
+    const siteCtx = ctxLib.get();
+
     const processor = graphQlRxLib.createPublishProcessor();
+
     eventLib.listener({
         type: '*',
         callback: function (event) {
-            if (!(event.type == 'application' && event.data.applicationKey == app.name)) { //TODO Temporary Workaround
-                processor.onNext(event);
-            }
+            context.options.subscriptionEventTypes.some(subscriptionEventType => {
+                if (subscriptionEventType === '*') {
+                    processor.onNext(event);
+                    return true;
+                } else {
+                    let eventType = subscriptionEventType.endsWith('*')
+                                    ? subscriptionEventType.substring(0, subscriptionEventType.length - 1)
+                                    : subscriptionEventType;
+
+                    let eventMatched = false;
+
+                    if (event.type.startsWith(eventType)) {
+                        eventMatched = isEventMatched(event, siteCtx, event.type);
+                    } else if (subscriptionEventType === event.type) {
+                        eventMatched = isEventMatched(event, siteCtx, subscriptionEventType);
+                    }
+
+                    if (eventMatched) {
+                        processor.onNext(event);
+                        return true;
+                    }
+                }
+            });
         }
     });
     return processor;
+}
+
+function isEventMatched(event, siteCtx, type) {
+    if (!type.startsWith("node.")) {
+        return true;
+    }
+
+    return event.data.nodes.some(node => {
+        if (node.repo === siteCtx.repository && node.branch === siteCtx.branch) {
+            return true;
+        }
+    });
 }
 
 exports.createEventObjectType = createEventObjectType;
