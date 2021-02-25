d2fb4b3ef08fa2e586c0eda5058e8f5d85e5cb5d
Anatol Sialitski <asi@enonic.com>
2021-02-25 15:45:12 +0300

    Add LocalDateTime, LocalTime and Date scalars #114

--- src/test/resources/lib/generic-form-test.js
+++ src/test/resources/lib/generic-form-test.js
@@ -9,6 +9,67 @@ exports.testGenerateFormItemObjectType = function () {
             maximum: 1
         }
     });
-
     testingLib.assertEquals('Attachment', objectType.name);
+
+    objectType = formLib.generateFormItemObjectType({}, 'namePrefix', {
+        formItemType: "Input",
+        name: "dateTimeField",
+        label: "DateTime Field",
+        maximize: true,
+        inputType: "DateTime",
+        occurrences: {
+            maximum: 1,
+            minimum: 0
+        },
+        config: {
+            timezone: [
+                {
+                    value: "true"
+                }
+            ]
+        }
+    });
+    testingLib.assertEquals('DateTime', objectType.name);
+
+    objectType = formLib.generateFormItemObjectType({}, 'namePrefix', {
+        formItemType: "Input",
+        name: "dateTimeField",
+        label: "DateTime Field",
+        maximize: true,
+        inputType: "DateTime",
+        occurrences: {
+            maximum: 1,
+            minimum: 0
+        },
+        config: {}
+    });
+    testingLib.assertEquals('LocalDateTime', objectType.name);
+
+    objectType = formLib.generateFormItemObjectType({}, 'namePrefix', {
+        formItemType: "Input",
+        name: "timeField",
+        label: "Time Field",
+        maximize: true,
+        inputType: "Time",
+        occurrences: {
+            maximum: 1,
+            minimum: 0
+        },
+        config: {}
+    });
+    testingLib.assertEquals('LocalTime', objectType.name);
+
+    objectType = formLib.generateFormItemObjectType({}, 'namePrefix', {
+        formItemType: "Input",
+        name: "dateField",
+        label: "Date Field",
+        maximize: true,
+        inputType: "Date",
+        occurrences: {
+            maximum: 1,
+            minimum: 0
+        },
+        config: {}
+    });
+    testingLib.assertEquals('Date', objectType.name);
 };
