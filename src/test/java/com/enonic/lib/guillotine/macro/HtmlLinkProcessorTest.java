fac5d3cb49e106a571933c20586092c1e471eb27
Anatol Sialitski <asi@enonic.com>
2021-02-25 17:38:40 +0300

    processHtml-less html area #112

--- src/test/java/com/enonic/lib/guillotine/macro/HtmlLinkProcessorTest.java
+++ src/test/java/com/enonic/lib/guillotine/macro/HtmlLinkProcessorTest.java
@@ -18,9 +18,12 @@ import com.enonic.xp.portal.url.PortalUrlService;
 import com.enonic.xp.site.Site;
 import com.enonic.xp.site.SiteConfig;
 import com.enonic.xp.site.SiteConfigs;
+import com.enonic.xp.style.ImageStyle;
+import com.enonic.xp.style.StyleDescriptor;
 import com.enonic.xp.style.StyleDescriptorService;
 import com.enonic.xp.style.StyleDescriptors;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -38,23 +41,40 @@ public class HtmlLinkProcessorTest
 
         HtmlLinkProcessor instance = new HtmlLinkProcessor( styleDescriptorService, portalUrlService );
 
-        String result = instance.process( "" +
-                                              "<figure class=\"editor-style-image-cinema\">" +
+        String result = instance.process( "" + "<figure class=\"editor-style-image-cinema\">" +
                                               "<img alt=\"matrix.jpg\" src=\"image://content-id?style=editor-style-image-cinema\" />" +
-                                              "<figcaption>Caption</figcaption>" +
-                                              "</figure>", "server", createPortalRequest(), Arrays.asList( 760, 1024 ), image -> {} );
+                                              "<figcaption>Caption</figcaption>" + "</figure>", "server", createPortalRequest(),
+                                          Arrays.asList( 760, 1024 ), image -> {
+            } );
 
-        assertTrue(result.contains( "<img alt=\"matrix.jpg\" src=\"imageUrl\" data-image-ref=" ));
-        assertTrue(result.contains( "srcset=\"imageUrl 760w,imageUrl 1024w\"" ));
+        assertTrue( result.contains( "<img alt=\"matrix.jpg\" src=\"imageUrl\" data-image-ref=" ) );
+        assertTrue( result.contains( "srcset=\"imageUrl 760w,imageUrl 1024w\"" ) );
 
-        result = instance.process( "" +
-                                       "<figure class=\"editor-style-image-cinema\">" +
+        result = instance.process( "" + "<figure class=\"editor-style-image-cinema\">" +
                                        "<img alt=\"matrix.jpg\" class=\"custom-class\" src=\"image://content-id?style=editor-style-image-cinema\" />" +
-                                       "<figcaption>Caption</figcaption>" +
-                                       "</figure>", "server", createPortalRequest(), Arrays.asList( 1024, 2048 ), image -> {} );
+                                       "<figcaption>Caption</figcaption>" + "</figure>", "server", createPortalRequest(),
+                                   Arrays.asList( 1024, 2048 ), image -> {
+            } );
 
-        assertTrue(result.contains( "<img alt=\"matrix.jpg\" class=\"custom-class\" src=\"imageUrl\" data-image-ref=" ));
-        assertTrue(result.contains( "srcset=\"imageUrl 1024w,imageUrl 2048w\"" ));
+        assertTrue( result.contains( "<img alt=\"matrix.jpg\" class=\"custom-class\" src=\"imageUrl\" data-image-ref=" ) );
+        assertTrue( result.contains( "srcset=\"imageUrl 1024w,imageUrl 2048w\"" ) );
+
+        when( styleDescriptorService.getByApplications( any( ApplicationKeys.class ) ) ).thenReturn(
+            StyleDescriptors.from( StyleDescriptor.create().
+                addStyleElement( ImageStyle.create().
+                    name( "editor-style-image-cinema" ).
+                    aspectRatio( "16:9" ).
+                    build() ).
+                application( ApplicationKey.from( "myapp" ) ).
+                build() ) );
+
+        result = instance.process( "" + "<figure class=\"editor-style-image-cinema\">" +
+                                       "<img alt=\"matrix.jpg\" class=\"custom-class\" src=\"image://content-id?style=editor-style-image-cinema\" />" +
+                                       "<figcaption>Caption</figcaption>" + "</figure>", "server", createPortalRequest(), null, image -> {
+        } );
+
+        assertTrue( result.contains( "<img alt=\"matrix.jpg\" class=\"custom-class\" src=\"imageUrl\" data-image-ref=" ) );
+        assertFalse( result.contains( "srcset=" ) );
     }
 
     private PortalRequest createPortalRequest()
