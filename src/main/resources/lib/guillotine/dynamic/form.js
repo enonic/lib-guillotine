413351158cef88b334197b068377dc057100568b
Anatol Sialitski <asi@enonic.com>
2021-03-17 14:44:14 +0300

    Rename HtmlEditorResult to HtmlAreaResult #130

--- src/main/resources/lib/guillotine/dynamic/form.js
+++ src/main/resources/lib/guillotine/dynamic/form.js
@@ -104,7 +104,7 @@ function generateInputObjectType(context, input) {
     case 'GeoPoint':
         return context.types.geoPointType;
     case 'HtmlArea':
-        return context.types.htmlEditorResultType;
+        return context.types.htmlAreaResultType;
     case 'ImageSelector':
         return graphQlLib.reference('Content');
     case 'ImageUploader':
